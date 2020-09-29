cc6e1f00fcfb4b75d821223f6358c615908314df
AggieWB <usmaggie23@gmail.com>
2020-09-28 19:53:00 -0700

    Add js file with a function that finds the sum of all the multiples
    of 3 or 5 below the provided parameter value number

---
+++ project-euler/multiples-of-3-and-5.js
@@ -0,0 +1,27 @@
+/*
+https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-1-multiples-of-3-and-5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+*/ 
+
+function multiplesOf3and5(number) {
+    let sum = 0;
+    if(number >= 3){
+      sum += 3;
+    }
+    if(number >= 5){
+      sum += 5;
+    }
+  
+    for(let i = 6; i < number; i++){
+      if(i % 3 === 0){
+        sum += i;
+      } else if(i % 5 === 0){
+        sum += i;
+      }
+    }
+    return sum;
+}
