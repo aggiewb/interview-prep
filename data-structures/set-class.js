bab813e468d489e3d75ed706d2877c7d64638fc8
AggieWB <usmaggie23@gmail.com>
2020-11-02 12:02:35 -0800

    Add an intersection() method to the Set class
    Update union() variable names

--- data-structures/set-class.js
+++ data-structures/set-class.js
@@ -19,9 +19,8 @@ we will create a size method that returns the number of elements inside the set 
 
 Create an add method that adds a unique value to the set collection and returns true if the value was successfully added and false otherwise.
 
-Create a remove method that accepts a value and checks if it exists in the set. 
-If it does, then this method should remove it from the set collection, and return true. Otherwise, it should return false. 
-Create a size method that returns the size of the set collection.
+Create a remove method that accepts a value and checks if it exists in the set. If it does, then this method should remove it from the set collection, 
+and return true. Otherwise, it should return false. Create a size method that returns the size of the set collection.
 
 https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/perform-a-union-on-two-sets
 
@@ -29,6 +28,14 @@ In this exercise we are going to perform a union on two sets of data. We will cr
 This method should take another Set as an argument and return the union of the two sets, excluding any duplicate values.
 
 For example, if setA = ['a','b','c'] and setB = ['a','b','d','e'], then the union of setA and setB is: setA.union(setB) = ['a', 'b', 'c', 'd', 'e'].
+
+https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/perform-an-intersection-on-two-sets-of-data
+
+In this exercise we are going to perform an intersection on 2 sets of data. We will create a method on our Set data structure called intersection. 
+An intersection of sets represents all values that are common to two or more sets. This method should take another Set as an argument and 
+return the intersection of the two sets.
+
+For example, if setA = ['a','b','c'] and setB = ['a','b','d','e'], then the intersection of setA and setB is: setA.intersection(setB) = ['a', 'b'].
 */
 
 class Set {
@@ -72,12 +79,30 @@ class Set {
 
     union(dictionaryToAdd){
       const newSet = new Set();
-      this.values().forEach(item => {
-        newSet.add(item);
+      this.values().forEach(value => {
+        newSet.add(value);
       });
-      dictionaryToAdd.values().forEach(item => {
-        newSet.add(item);
+      dictionaryToAdd.values().forEach(value => {
+        newSet.add(value);
       });
+      return newSet;
+    }
+
+    intersection(dictionaryToIntersect){
+      const newSet = new Set();
+      if(dictionaryToIntersect.size() > this.size()){
+        dictionaryToIntersect.values().forEach(value => {
+          if(this.has(value)){
+            newSet.add(value);
+          }
+        });
+      } else {
+        this.values().forEach(value => {
+          if(dictionaryToIntersect.has(value)){
+            newSet.add(value);
+          }
+        });
+      }
       return newSet;
     }
 }
