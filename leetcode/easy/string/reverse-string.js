95e6737d9c7842ac032959cca2f0eb1b77f4ac72
AggieWB <usmaggie23@gmail.com>
2021-01-12 09:14:49 -0800

    Add function from previous commit mistakenly missed

---
+++ leetcode/easy/string/reverse-string.js
@@ -0,0 +1,8 @@
+const reverseString = function(s) {
+    const lastIndex = s.length - 1;
+    for(let i = 0; i < s.length / 2; i++){
+        const temp = s[i];
+        s[i] = s[lastIndex - i];
+        s[lastIndex - i] = temp;
+    }
+};
