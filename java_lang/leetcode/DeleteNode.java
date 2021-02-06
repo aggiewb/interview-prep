0aa96b48c4c975b60c2e216e672e8a306aec56c0
AggieWB <usmaggie23@gmail.com>
2021-02-06 10:20:20 -0800

    Add files and methods for java and js deleting a node in a
    singly linked list

---
+++ java_lang/leetcode/DeleteNode.java
@@ -0,0 +1,19 @@
+package java_lang.leetcode;
+
+/**
+ * https://leetcode.com/problems/delete-node-in-a-linked-list/solution/
+ * 
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public void deleteNode(ListNode node){
+        node.val = node.next;
+        node.next = node.next.next;
+    }
+}
---
+++ javascript/leetcode/easy/linked-list/delete-node.js
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.com/problems/delete-node-in-a-linked-list/submissions/
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+
+ const deleteNode = function(node){
+     node.val = node.next.val;
+     node.next = node.next.next;
+ }
