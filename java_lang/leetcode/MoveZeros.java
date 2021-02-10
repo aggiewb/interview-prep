70ecae0280e736edda20e5260bf349cecbd02092
AggieWB <usmaggie23@gmail.com>
2021-02-10 10:03:20 -0800

    Add commenting to leetcode problem url

--- java_lang/leetcode/MoveZeros.java
+++ java_lang/leetcode/MoveZeros.java
@@ -1,5 +1,9 @@
 package java_lang.leetcode;
 
+/*
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/
+*/ 
+
 public class MoveZeros {
     public void moveZeroes(int[] nums){
         int numsLength = nums.length;
