3263b8d40ffc8a8bbd9f02e79716b0d93e44ab7e
AggieWB <usmaggie23@gmail.com>
2021-02-10 10:04:00 -0800

    Add a java file with a method that reverses a string

---
+++ java_lang/leetcode/ReverseString.java
@@ -0,0 +1,18 @@
+package java_lang.leetcode;
+
+/*
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/
+*/ 
+
+public class ReverseString {
+    public void reverseString(char[] s){
+        int sLength = s.length;
+        int sLastIndex = sLength - 1;
+        for(int i = 0; i < sLength / 2; i++){
+            char temp = s[i];
+            s[i] = s[sLastIndex - i];
+            s[sLastIndex - i] = temp;
+        }
+    }
+    
+}
