e34e321c9fff5ffbf0c3ce93ab3325993ceff24c
AggieWB <usmaggie23@gmail.com>
2021-02-11 14:31:22 -0800

    Add a java file with a method that given the root of a binary
    tree, returns its maximum depth

---
+++ java_lang/leetcode/MaxDepthBinaryTree.java
@@ -0,0 +1,29 @@
+package java_lang.leetcode;
+
+/*
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/555/
+
+ Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+*/ 
+
+public class MaxDepthBinaryTree {
+    public int maxDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        } else {
+            return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+        }
+    }
+}
