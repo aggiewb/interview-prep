5908b8fe2abd8c6cff14dfe5620d920017019bcb
AggieWB <usmaggie23@gmail.com>
2021-02-12 09:17:13 -0800

    Add a java file with a method that  outputs the string
    representation of numbers from 1 to n, but for multiples of three it outputs “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five outputs “FizzBuzz”.

---
+++ java_lang/leetcode/FizzBuzz.java
@@ -0,0 +1,29 @@
+package java_lang.leetcode;
+import java.util.*;
+
+/*
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/743/
+*/ 
+
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> fizzBuzz = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i++){
+            String word = "";
+            if(i % 3 == 0){
+                word += "Fizz";
+            }
+            if(i % 5 == 0){
+                word += "Buzz";
+            }
+            
+            if(word.isEmpty()){
+                fizzBuzz.add(i + "");
+            } else {
+                fizzBuzz.add(word);
+            }
+        }
+        return fizzBuzz;
+    }
+}
