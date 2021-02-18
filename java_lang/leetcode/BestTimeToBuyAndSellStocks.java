05cd3e7ed2b0618f14f7ead3cba9e5b61ee2dfa7
AggieWB <usmaggie23@gmail.com>
2021-02-18 10:06:48 -0800

    Add a java file with a method that given array prices for which the ith element is the price of a given stock on day i finds the maximum profit

---
+++ java_lang/leetcode/BestTimeToBuyAndSellStocks.java
@@ -0,0 +1,21 @@
+package java_lang.leetcode;
+
+/*
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+*/ 
+
+class BestTimeToBuyAndSellStocks {
+    public int maxProfit(int[] prices){
+        int profit = 0;
+
+        for(int i = 0; i < prices.length - 1; i++){
+            int buyPrice = prices[i];
+            int sellPrice = prices[i + 1];
+
+            if(sellPrice > buyPrice){
+                profit += sellPrice - buyPrice;
+            }
+        }
+        return profit;
+    }
+}
