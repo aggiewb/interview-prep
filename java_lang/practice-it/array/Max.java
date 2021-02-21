9bd252bc8c76294c46d8edd9a8b4cd9dbe77325f
AggieWB <usmaggie23@gmail.com>
2021-02-21 09:50:57 -0800

    Add a java file with a method that accepts an array of integers
    as a parameter and returns the maximum value in the array

---
+++ java_lang/practice-it/array/Max.java
@@ -0,0 +1,17 @@
+
+/*
+https://practiceit.cs.washington.edu/problem/view/bjp5/chapter7/s10%2Dmax
+*/ 
+
+public class Max {
+    public static int max(int[] nums){
+        int maxValue = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            int compareNum = nums[i];
+            if(compareNum > maxValue){
+                maxValue = compareNum;
+            }
+        }
+        return maxValue;
+    }
+}
