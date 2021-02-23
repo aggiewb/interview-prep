e8d751c3883db010e97e678c0f0681cd4bd2e76b
AggieWB <usmaggie23@gmail.com>
2021-02-22 18:03:34 -0800

    Add a java file with a method that given a sorted array of
    distinct integers and a target value, returns the index if the target is found. If not, returns the index where it would be if it were inserted in order.

---
+++ java_lang/leetcode/SearcgInsertPosition.java
@@ -0,0 +1,23 @@
+package java_lang.leetcode;
+
+/*
+https://leetcode.com/problems/search-insert-position/
+*/ 
+
+public class SearcgInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        if(nums[0] >= target){
+            return 0;
+        }
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == target){
+                return i;
+            } else if(nums[i + 1] == target){
+                return i + 1;
+            } else if(nums[i + 1] > target){
+                return i + 1;
+            }
+        }
+        return nums.length;
+    }
+}
