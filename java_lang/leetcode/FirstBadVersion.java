590b94707558a62e3ac02226462526280cd7b53c
AggieWB <usmaggie23@gmail.com>
2021-02-23 14:45:27 -0800

    Add a java file with a method that given an API bool
    isBadVersion(version) which returns whether version is bad, returns the first bad version

---
+++ java_lang/leetcode/FirstBadVersion.java
@@ -0,0 +1,31 @@
+package java_lang.leetcode;
+
+/*
+https://leetcode.com/problems/first-bad-version/
+
+The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version);
+*/ 
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while(left < right){
+            //algorithm for finding midpoint that isn't affected by the overflow bug
+            int mid = left + (right - left) / 2;
+            if(isBadVersion(mid)){
+                //example:
+                // 1 2 3 4 5 6 7 8 9
+                // G G G B B B B B B   
+                right = mid;
+            } else {
+                //example:
+                // 1 2 3 4 5 6 7 8 9
+                // G G G G G G B B B
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+}
