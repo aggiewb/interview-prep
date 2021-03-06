cd6c4bede6ec9218dad09d1bf095bbd3ff754e42
AggieWB <usmaggie23@gmail.com>
2021-03-06 14:26:37 -0800

    Add java file with example of data type initializations
    Add array initialization examples

---
+++ java_lang/other_problems/InitializationGuide.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class InitializationGuide {
+    public static void main(String[] args){
+        int size = 10;
+        //Array
+        int[] nums1Array = new int[size];
+        int[] nums2Array = {1, 2, 3};
+        String[] word1Array = new String[10];
+        String[] word2Array = {"Aggie", "Steven", "Frank"};
+    }
+    
+}
