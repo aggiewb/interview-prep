354992d1b9af6938cf18bb38e13990da69db9bbb
AggieWB <usmaggie23@gmail.com>
2021-03-06 14:46:58 -0800

    Add multidimension array examples
    Add ArrayList initialization examples

--- java_lang/other_problems/InitializationGuide.java
+++ java_lang/other_problems/InitializationGuide.java
@@ -2,12 +2,22 @@ import java.util.*;
 
 public class InitializationGuide {
     public static void main(String[] args){
-        int size = 10;
         //Array
+        int size = 10;
         int[] nums1Array = new int[size];
         int[] nums2Array = {1, 2, 3};
+        int[][] multi1Dimension = new int[size][size];
         String[] word1Array = new String[10];
         String[] word2Array = {"Aggie", "Steven", "Frank"};
+        String[][] multi2Dimension = {{"Milk", "Cream"}, {"Chicken", "Beef"}};
+        
+        //ArrayList
+        ArrayList<String> wordArrayList = new ArrayList<>();
+        ArrayList<Integer> numArrayList = new ArrayList<>();
+        List<Character> charArrayList = Arrays.asList('a', 'b', 'c');
+        
+        Collection<Integer> strCollection = new HashSet<>();
+        ArrayList<Integer> collection = new ArrayList<Integer>(strCollection);
     }
     
 }
