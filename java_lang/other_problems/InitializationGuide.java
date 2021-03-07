cb0cd6f222cdd7c37ecfc9a00af0b86ac15aace2
AggieWB <usmaggie23@gmail.com>
2021-03-07 14:22:34 -0800

    Add examples for LinkedList initialization
    Add commenting with primitive and wrapper class types

--- java_lang/other_problems/InitializationGuide.java
+++ java_lang/other_problems/InitializationGuide.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class InitializationGuide {
     public static void main(String[] args){
-        //Primitive Data Types: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+        //Primitive Data Types: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html (byte, short, int, long, float, double, boolean, char)
+        //Wrapper Classes: Byte, Short, Integer, Long, Float, Double, Boolean, Character
         //String: https://docs.oracle.com/javase/8/docs/api/java/lang/String.html
         //Math: https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html
         //Number: https://docs.oracle.com/javase/8/docs/api/java/lang/Number.html 
@@ -25,10 +26,16 @@ public class InitializationGuide {
         List<Character> charArrayList = Arrays.asList('a', 'b', 'c');
 
         Collection<Integer> strCollection = new HashSet<>();
-        ArrayList<Integer> collection = new ArrayList<Integer>(strCollection);
+        ArrayList<Integer> collection = new ArrayList<>(strCollection);
 
         //LinkedList
         //https://docs.oracle.com/javase/8/docs/api/java/util/LinkedList.html
+        List<Integer> example1 = new LinkedList<>(Arrays.asList(1, 2));
+        List<Character> example2 = new LinkedList<>();
+        LinkedList<String> example3 = new LinkedList<>();
+        
+        ArrayList<Float> floatArrayList = new ArrayList<>();
+        LinkedList<Float> example4 = new LinkedList<>(floatArrayList);
 
         //Set: HashSet and TreeSet flavors
         //https://docs.oracle.com/javase/8/docs/api/java/util/Set.html
